f8a4383e85b14d751ac1ab0d95e79faec0088b7f
ggail01 <tyzmaineleandre@gmail.com>
2024-01-11 16:00:12 +0800

    homepage

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,2 @@
-document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
