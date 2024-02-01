d91a52d31c9179f3049454704c322c2061b821dd
ggail01 <tyzmaineleandre@gmail.com>
2024-02-01 16:45:10 +0800

    homepage

--- scripts/form.js
+++ scripts/form.js
@@ -1,15 +1,3 @@
-// Password
-function validatePasswords() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-
-    if (password !== confirmPassword) {
-        document.getElementById("confirmPassword").setCustomValidity("Passwords do not match");
-    } else {
-        document.getElementById("confirmPassword").setCustomValidity("");
-    }
-}
-
 // Show Password
 function togglePasswordVisibility(inputId) {
     var passwordInput = document.getElementById(inputId);
@@ -22,10 +10,33 @@ function togglePasswordVisibility(inputId) {
         passwordInput.type = "password";
         showPasswordBtn.textContent = "Show";
     }
+
+    validatePasswords(); // Trigger password validation after toggling visibility
+}
+
+// Matching Password
+function validatePasswords() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var passwordMatchStatus = document.getElementById("passwordMatchStatus");
+
+    if (password === confirmPassword) {
+        passwordMatchStatus.textContent = "Passwords match!";
+        passwordMatchStatus.style.color = "#3a9adf";
+    } else {
+        passwordMatchStatus.textContent = "Passwords do not match.";
+        passwordMatchStatus.style.color = "red";
+    }
 }
 
 // Rating Value
-function updateRatingValue() {
-    var ratingValue = document.getElementById("pageRating").value;
-    document.getElementById("ratingValue").innerText = ratingValue;
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
 }
