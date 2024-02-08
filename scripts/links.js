535c595c6da2e873d33941e7c2ec3b3b20af45bf
ggail01 <tyzmaineleandre@gmail.com>
2024-02-08 12:30:22 +0800

    homepage

---
+++ scripts/links.js
@@ -0,0 +1,26 @@
+const activitiesList = document.querySelector('#links');
+const baseURL = "https://ggail01.github.io/wdd230/";
+const linksURL = "https://ggail01.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+} 
+
+function displayLinks(weeks) {
+    weeks.forEach((week) => {
+        const li = document.createElement('li');
+        li.textContent = `${week.week} `;
+        week.links.forEach(link => {
+            const a = document.createElement('a');
+            a.setAttribute('href', baseURL + link.url);
+            a.setAttribute('target', '_blank');
+            a.textContent = `${link.title} | `;
+            li.appendChild(a);
+        });
+        activitiesList.appendChild(li);
+    });
+}
+
+getLinks();
---
+++ scripts/weather.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '1d3a20aeacdc22dc3e6e761d3de471f8';
+
+async function apiFetch() {
+    const latitude = 14.67;
+    const longitude = 121.03;
+
+    const apiUrl = `${url}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}°F`;
+    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('SRC', iconSrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
